a490152c0fee42430f122c1427111e05b435e9a1
katethequeen <cateresa4@gmail.com>
2024-12-01 14:23:40 +0100

    add footer component

---
+++ src/app/components/nav-genres/nav-genres.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-genres',
+  templateUrl: './nav-genres.component.html',
+  styleUrl: './nav-genres.component.scss'
+})
+export class NavGenresComponent {
+
+}
